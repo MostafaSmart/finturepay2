51e5f0d385d61604ab62d55b460a0e9f70a46aed
mostafasmart <clashmostafa465@gmail.com>
2026-03-08 00:56:31 +0300

    .

--- data/cone.php
+++ data/cone.php
@@ -1,9 +1,9 @@
 <?php
 // بيانات الاتصال بقاعدة البيانات
-$DB_HOST = "localhost";
-$DB_USER = "root";
-$DB_PASS = "";
-$DB_NAME = "app_link";
+$DB_HOST = "sql112.yzz.me";
+$DB_USER = "yzzme_41332687";
+$DB_PASS = "777296572mosmos";
+$DB_NAME = "yzzme_41332687_app_link";
 $DB_PROT = 3306;
 
 // إنشاء الاتصال
